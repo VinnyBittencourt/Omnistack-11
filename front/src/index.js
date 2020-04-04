36316aca3c593c5d8e5c82f2a0fc649292a4ff08
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-04-04 18:35:02 -0300

    frontend new folder

---
+++ front/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root")); // Onde renderiza no index.html o conteudo jsx (Javascript e XML (Que é a sintaxe do HTML)) que está no arquivo App.js
